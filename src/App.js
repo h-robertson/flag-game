eddbc1a950af56f6bcb60896021fe847d87b6c51
Helena <h.robertson@icloud.com>
2022-07-01 12:44:56 +0100

    counter test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react'
 
 function App() {
+	const [counter, setCounter] = useState(0)
+
+
+	useEffect(()=>{
+		console.log("counter changed", counter)
+	}, [counter])
+
+	function incrementCounter(){
+		setCounter(counter+1)
+	}
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {counter > 4 ? <p> greater </p> : counter}
         </p>
-        <a
+        <button
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
           rel="noopener noreferrer"
+		  onClick={incrementCounter}
         >
-          Learn React
-        </a>
+          {counter}
+        </button>
       </header>
     </div>
   );
